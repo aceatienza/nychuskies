5e3e6abe6993dab3ca1ce8429bc9f1021946db52
Ace Atienza <aceatienza@about.me>
2013-04-20 00:21:39 -0400

    add site nav to bottom of page

--- footer.php
+++ footer.php
@@ -10,13 +10,22 @@
 	 * @since Twenty Twelve 1.0
 	 */
 	?>
+
+		<div class="container" style="margin: 0 auto; width: 50%;">
+			<div class="row">
+				<div class="span2"><?php dynamic_sidebar( 'sidebar-2' ); ?></div>  <!-- Left column: Categories -->
+				<div class="span2"><?php dynamic_sidebar( 'sidebar-3' ); ?></div>  <!-- Middle column: Archives -->
+				<div class="span2"><?php dynamic_sidebar( 'sidebar-4' ); ?></div>  <!-- Right column: Meta -->
+			</div>
+		</div>
+
 		</div><!-- #main .wrapper -->
 
-		<div class="pull-right">Design by <a href="<?php echo esc_url('www.linkedin.com/in/aceatienza/'); ?>">Ace Atienza</a> '02</div>
 
 
+	<div class="pull-right">Design by <a href="<?php echo esc_url('www.linkedin.com/in/aceatienza/'); ?>">Ace Atienza</a> '02</div>
 	</div><!-- #page -->
-
+	
 	<?php wp_footer(); ?>
 	</body>
 </html>
