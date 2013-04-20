ea685af626eba229d5305e3681c44c442420024c
Ace Atienza <aceatienza@about.me>
2013-04-20 13:01:33 -0400

    move bootstrap above wp styles to load properly. finally get rid of important in style.css

--- functions.php
+++ functions.php
@@ -22,6 +22,33 @@
  * @since Twenty Twelve 1.0
  */
 
+/*
+ * Load Bootstrap and add other js files here
+*/
+function theme_js()
+{
+	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '2.3.1' );
+	wp_enqueue_script( 'bootstrap-js' );
+}
+add_action( 'wp_enqueue_scripts', 'theme_js' );
+
+/**
+* Load CSS styles 
+*/
+function theme_styles()
+{
+    wp_register_style( 'bootstrap-responsive', get_template_directory_uri() . '/bootstrap/css/bootstrap-responsive.min.css', array(), '2.3.1', 'all' );
+    wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array(), '2.3.1', 'all' );
+
+    wp_enqueue_style('bootstrap-responsive');
+    wp_enqueue_style('bootstrap-css');
+}
+add_action('wp_enqueue_scripts', 'theme_styles');
+
+// Register Custom Navigation Walker
+require_once('twitter_bootstrap_nav_walker.php');
+
+
 /**
  * Sets up the content width value based on the theme's design and stylesheet.
  */
@@ -103,42 +130,6 @@ function twentytwelve_scripts_styles() {
 	wp_enqueue_script( 'twentytwelve-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
 	*/
 
-	/*
-	 * Loads our special font CSS file.
-	 *
-	 * The use of Open Sans by default is localized. For languages that use
-	 * characters not supported by the font, the font can be disabled.
-	 *
-	 * To disable in a child theme, use wp_dequeue_style()
-	 * function mytheme_dequeue_fonts() {
-	 *     wp_dequeue_style( 'twentytwelve-fonts' );
-	 * }
-	 * add_action( 'wp_enqueue_scripts', 'mytheme_dequeue_fonts', 11 );
-	 */
-
-	/* translators: If there are characters in your language that are not supported
-	   by Open Sans, translate this to 'off'. Do not translate into your own language. */
-	if ( 'off' !== _x( 'on', 'Open Sans font: on or off', 'twentytwelve' ) ) {
-		$subsets = 'latin,latin-ext';
-
-		/* translators: To add an additional Open Sans character subset specific to your language, translate
-		   this to 'greek', 'cyrillic' or 'vietnamese'. Do not translate into your own language. */
-		$subset = _x( 'no-subset', 'Open Sans font: add new subset (greek, cyrillic, vietnamese)', 'twentytwelve' );
-
-		if ( 'cyrillic' == $subset )
-			$subsets .= ',cyrillic,cyrillic-ext';
-		elseif ( 'greek' == $subset )
-			$subsets .= ',greek,greek-ext';
-		elseif ( 'vietnamese' == $subset )
-			$subsets .= ',vietnamese';
-
-		$protocol = is_ssl() ? 'https' : 'http';
-		$query_args = array(
-			'family' => 'Open+Sans:400italic,700italic,400,700',
-			'subset' => $subsets,
-		);
-		wp_enqueue_style( 'twentytwelve-fonts', add_query_arg( $query_args, "$protocol://fonts.googleapis.com/css" ), array(), null );
-	}
 
 	/*
 	 * Loads our main stylesheet.
@@ -460,28 +451,3 @@ function twentytwelve_customize_preview_js() {
 }
 add_action( 'customize_preview_init', 'twentytwelve_customize_preview_js' );
 
-/*
- * Load Bootstrap and add other js files here
-*/
-function theme_js()
-{
-	wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '2.3.1' );
-	wp_enqueue_script( 'bootstrap-js' );
-}
-add_action( 'wp_enqueue_scripts', 'theme_js' );
-
-/**
-* Load CSS styles 
-*/
-function theme_styles()
-{
-    wp_register_style( 'bootstrap-responsive', get_template_directory_uri() . '/bootstrap/css/bootstrap-responsive.min.css', array(), '2.3.1', 'all' );
-    wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css', array(), '2.3.1', 'all' );
-
-    wp_enqueue_style('bootstrap-responsive');
-    wp_enqueue_style('bootstrap-css');
-}
-add_action('wp_enqueue_scripts', 'theme_styles');
-
-// Register Custom Navigation Walker
-require_once('twitter_bootstrap_nav_walker.php');
