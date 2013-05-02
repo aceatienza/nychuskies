27f3469e217919d6b06bd7015ab5be110ce7a819
Ace Atienza <aceatienza@about.me>
2013-05-01 23:02:04 -0400

    adjust height on sticky navbar for best look; doesn't work as well on chrome

--- js/fixednav.js
+++ js/fixednav.js
@@ -1,10 +1,10 @@
 /* Fix nav bar to the top of all pages
 */
 var fixednav = document.querySelector('.navbar');
-var origOffsetY = fixednav.offsetTop;
+var carouselHeight = (document.getElementById("myCarousel").offsetHeight) + 10; // should be (document.getElementById("thin-strip").offsetHeight) // but doesn't work properly
 
 function onScroll(e) {
-  window.scrollY >= origOffsetY ? fixednav.classList.add('fixed') : fixednav.classList.remove('fixed');
+  window.scrollY >= carouselHeight ? fixednav.classList.add('fixed') : fixednav.classList.remove('fixed');
 }
 
 document.addEventListener('scroll', onScroll);
